a0b74be7cc960d532e7d5fd1afb0fbb54bbb27de
Christopher.L.Zeig11 <Christopher.L.Zeig11@BusElabTwo.BUSINESS.ColoState.EDU>
2013-03-17 09:36:26 -0600

    Added error checking to see if the material was already being used in the project before allowing it to be added.

--- addMaterial.php
+++ addMaterial.php
@@ -27,23 +27,36 @@ if(! empty($_POST)) {
         $errorMessages .= "<li>Material Quantity is a required field.</li>";
     }
 
-    echo($errorMessages);
+    
 
     if(empty($errorMessages)) {
         // make sure that the user name is not already being used
         $conn = dbConnect();
 
-        $query = $conn->prepare('insert into projectMaterial (projectId, materialId, number, cost) values (:projectId, :materialId, :materialQuantity, :cost)');
+        $query = $conn->prepare('select projectId, materialId from projectMaterial where projectId = :projectId and materialId = :materialId');
         $query->bindParam(':projectId', trim($projectId));
         $query->bindParam(':materialId', trim($materialId));
-        $query->bindParam(':materialQuantity', trim($materialQuantity));
-        $query->bindParam(':cost', trim($materialCost));
 
         $query->execute();
+        $results = $query->fetchAll(PDO::FETCH_ASSOC);  //retreive the rows as an associative array
 
+        if(count($results) > 0) {
+            $errorMessages .= "<li>That Material is already being used. Please choose another.</li>";
+        } else {
+        
+            $query = $conn->prepare('insert into projectMaterial (projectId, materialId, number, cost) values (:projectId, :materialId, :materialQuantity, :cost)');
+            $query->bindParam(':projectId', trim($projectId));
+            $query->bindParam(':materialId', trim($materialId));
+            $query->bindParam(':materialQuantity', trim($materialQuantity));
+            $query->bindParam(':cost', trim($materialCost));
+
+            $query->execute();
+        }
         dbDisconnect($conn);
 
     }        
+    
+    echo($errorMessages);
 }
 
 
