5edc3eb1e9631aa75eeb69daf478e570c1f12d7a
christopher.m.alle11 <christopher.m.alle11@BusElabTwo.BUSINESS.ColoState.EDU>
2013-03-17 10:34:16 -0600

    TEst

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
    <p>Home Page Text Goes Here</p>
    
     <h3>Header 3</h3>
-<p>Test Text! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat commodo nisl, ut lacinia erat vulputate ac. Integer porta est aliquam elit faucibus dictum id sed erat. Integer luctus diam eu justo dignissim scelerisque. Integer quis tellus in nisl dapibus gravida. Nunc at pellentesque est. Phasellus at imperdiet arcu. Proin felis nisi, congue non feugiat non, dictum nec mauris. Quisque condimentum, velit a luctus elementum, mi diam elementum dui, ac laoreet dolor orci non libero. Etiam placerat pretium erat vitae lobortis. Etiam sed tincidunt dolor.</p>
+<p>Test Text! Tesst 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat commodo nisl, ut lacinia erat vulputate ac. Integer porta est aliquam elit faucibus dictum id sed erat. Integer luctus diam eu justo dignissim scelerisque. Integer quis tellus in nisl dapibus gravida. Nunc at pellentesque est. Phasellus at imperdiet arcu. Proin felis nisi, congue non feugiat non, dictum nec mauris. Quisque condimentum, velit a luctus elementum, mi diam elementum dui, ac laoreet dolor orci non libero. Etiam placerat pretium erat vitae lobortis. Etiam sed tincidunt dolor.</p>
 <p>Pellentesque placerat, elit sed commodo imperdiet, lacus mi feugiat ante, vel tincidunt neque orci id tellus. Proin tempus malesuada rutrum. Integer velit leo, rutrum id laoreet ut, iaculis non odio. Cras nec lacus non arcu ornare egestas. Vivamus vel magna felis, eget vestibulum eros. Aliquam quis ipsum vel sem vehicula viverra. Donec id tellus vel sem laoreet tincidunt sit amet vel augue.</p>
   </div>
 
