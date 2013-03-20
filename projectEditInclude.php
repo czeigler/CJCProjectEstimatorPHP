8cf2cb2ba4294f8f6745cc8c87ec31199de1b786
Christopher.L.Zeig11 <Christopher.L.Zeig11@BuseLabFour.BUSINESS.ColoState.EDU>
2013-03-19 22:03:45 -0600

    Materials adding with error checking working.

--- projectEditInclude.php
+++ projectEditInclude.php
@@ -8,39 +8,53 @@
 ?>
 
 <h1><?php echo($projectName); ?></h1>
-    
+
+
+    <?php
+    if(! empty($errorMessages)) {
+        echo('<ul class="errorMessage">');
+        echo($errorMessages); 
+        echo('</ul><br/>');
+    }
+    ?>
+
 <h3>Manage Materials</h3>
 <form method="post" action="addMaterial.php">
     Material: 
     <select name="materialId">
+        <option value="">Please Select a Material</option>
         <?php foreach(getMaterialsList() as $material) {
-            extract($material);
-            var_dump($material);
-            echo "<option value='$materialUnitId'>$name</option>";
+            if(! is_null($materialId) && $materialId == $material['materialId']) {
+                echo "<option value='" . $material['materialId'] . "' selected='true'>" . $material['name'] . "</option>";
+            } else {
+                echo "<option value='" . $material['materialId'] . "'>" . $material['name'] . "</option>";
+            }
         }?>
     </select><br/>
     
-    Material Cost: <input type="text" name="cost" value="<?php echo($cost);?>"/><br/>
-    Material Quantity: <input type="text" name="quantity" value="<?php echo($quantity);?>"/><br/>
-    <input type="hidden" name="projectId" value="<?php echo($projectId);?>"/>
+    Material Cost: <input type="text" name="cost" value="<?php echo($materialCost);?>"/><br/>
+    Material Quantity: <input type="text" name="quantity" value="<?php echo($materialQuantity);?>"/><br/>
+    <input type="hidden" name="id" value="<?php echo($projectId);?>"/>
     <input type="submit" value="Add Material"/>
 </form>
 <hr/>
 
 <h3>Manage Labor</h3>
 <form method="post" action="addLabor.php">
-    Description: <input type="text" name="description" value="<?php echo($description);?>"/><br/>
-    Labor Hours: <input type="text" name="hours" value="<?php echo($hours);?>"/><br/>
-    Labor Rate: <input type="text" name="rate" value="<?php echo($rate);?>"/><br/>
-    <input type="hidden" name="projectId" value="<?php echo($projectId);?>"/>
+    Description: <input type="text" name="laborDescription" value="<?php echo($laborDescription);?>"/><br/>
+    Labor Hours: <input type="text" name="hours" value="<?php echo($laborHours);?>"/><br/>
+    Labor Rate: <input type="text" name="rate" value="<?php echo($laborRate);?>"/><br/>
+    <input type="hidden" name="id" value="<?php echo($projectId);?>"/>
     <input type="submit" value="Add Labor"/>
 </form>
 <hr/>
 
 <h3>Current Materials List</h3>
 
+
+
 <form method="post" action="calculateTotals.php">
-    <input type="hidden" name="projectId" value="<?php echo($projectId);?>"/>
+    <input type="hidden" name="id" value="<?php echo($projectId);?>"/>
     <input type="submit" value="Calculate Project Totals"/>
 </form>
 
